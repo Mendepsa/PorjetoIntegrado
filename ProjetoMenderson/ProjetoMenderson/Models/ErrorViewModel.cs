fe4a56fc7f2986fbef6c4312f0b26d681df3dd4d
Mendepsa <menderson.santos@aluno.epsa.com.br>
2023-05-30 01:34:07 -0300

    Primeiro commit

---
+++ ProjetoMenderson/ProjetoMenderson/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoMenderson.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
